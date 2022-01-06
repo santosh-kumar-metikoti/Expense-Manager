03ee6c4bc4913abbcda16589fb5cb85d65f8d367
santosh-kumar-metikoti <santosh.metikoti@gmail.com>
2022-01-06 14:40:05 +0530

    fixes #5(short abrrevations removed)

---
+++ WebApplication4/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication4.Models
+{
+        public class Account_list
+
+        {
+            public int AccountId { get; set; }
+
+            public string AccountName { get; set; }
+
+        }
+}
