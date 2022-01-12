b093d081d7d98f3678677ef943462030ae323991
santosh-kumar-metikoti <santosh.metikoti@gmail.com>
2022-01-12 08:57:45 +0530

    fixes #8

---
+++ WebApplication4/Models/AddAccountViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class AddAccountViewModel
+    {
+        public string MyRadioField { get; set; }
+        [Required]
+        public string incomeType { get; set; }
+
+    }
+}
