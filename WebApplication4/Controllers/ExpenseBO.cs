e8b6058e04f9418a7c873dabaa0669a7eaa2a603
santosh-kumar-metikoti <santosh.metikoti@gmail.com>
2022-02-16 09:33:12 +0530

    Tried adding validations

--- WebApplication4/Controllers/ExpenseBO.cs
+++ WebApplication4/Controllers/ExpenseBO.cs
@@ -1,32 +1,102 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using System.ComponentModel.DataAnnotations;
 using WebApplication4.Models;
 
 namespace WebApplication4.Controllers
 {
-    public class ExpenseBO : Controller
-    {
+    public class ExpenseBO
+	{
 		NpgsqlConnection connection;
 		//constructor
 		public ExpenseBO(NpgsqlConnection connection)
 		{
 			this.connection = connection;
 		}
-		public void MakeExpense(ExpensePayload payload)
+		private void TransactionValidations(MakeExpensePayload payload)
 		{
-			var trA = new TransactionAccessController(connection);
-			var acA = new AccountAccessController(connection);
-			//acA.MakeAccount(payload.Account);
-			var accountId = acA.GetAccountId(payload.Account);
-			Console.WriteLine(accountId);
-			payload.Transaction.Account_Id = accountId;
-			trA.MakeTransaction(payload.Transaction);
+			var exceptions = new List<Exception>();
+			foreach (var item in exceptions)
+			{
+				try
+				{
+					if (payload.Amount <= 0)
+					{
+						throw new ArgumentException();
+					}
+
+					if (string.IsNullOrEmpty(payload.Note))
+					{
+						throw new ArgumentException();
+					}
+
+					if (string.IsNullOrEmpty(payload.AccountName))
+					{
+						throw new ArgumentException();
+					}
+
+				}
+				catch (Exception ex)
+				{
+					exceptions.Add(ex);
+				}
+			}
+
+			if (exceptions.Count > 0)
+				throw new AggregateException(
+					"Encountered errors while trying to do something.",
+					exceptions
+				);
 
 		}
-		public void CreateAccount(ExpensePayload payload)
+
+
+		private void AccountValidations(CreateAccountPayload payload)
+		{
+			var exceptions = new List<ArgumentException>();
+			try
+			{
+				if (connection.ExecuteScalar<int?>(@"SELECT count(*) FROM account AS a WHERE a.account_name = @accountname",
+									new { payload.AccountName }) > 1)
+				{
+					throw new ArgumentException($"Account already exists with name {payload.AccountName}");
+				}
+				if (string.IsNullOrEmpty(payload.AccountName))
+				{
+					throw new ArgumentException($"Account Name cnnot be Empty");
+				}
+				if (string.IsNullOrEmpty(payload.Type))
+				{
+					throw new ArgumentException($"Account Type cnnot be Empty");
+				}
+			}
+			catch (ArgumentException ex)
+			{
+				exceptions.Add(ex);
+			}
+
+			if (exceptions.Count > 0)
+				throw new AggregateException(
+					"Encountered errors: ",
+					exceptions
+				);
+		}
+		public void MakeExpense(MakeExpensePayload payload)
+		{
+			TransactionValidations(payload);
+			var transactionAccess = new TransactionAccessController(connection);
+				var accountAccess = new AccountAccessController(connection);
+				//acA.MakeAccount(payload.Account);
+				var accountId = accountAccess.GetAccountId(payload.AccountName);
+				transactionAccess.MakeTransaction(payload);
+		}
+		public void CreateAccount(CreateAccountPayload payload)
         {
-			var acA = new AccountAccessController(connection);
-			acA.MakeAccount(payload.Account);
+			AccountValidations(payload);
+			var accountAccess = new AccountAccessController(connection);
+			accountAccess.MakeAccount(payload.AccountName,payload.Type);
 		}
 	}
+
 }
